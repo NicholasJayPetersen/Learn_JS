078093d255c089b13a8cd1fa5a061cec9aef0ac4
Nicholas Petersen <njpetersen@hawkmail.hfcc.edu>
2025-02-05 14:26:15 -0500

    Add basic alert lesson

---
+++ hello_world.js
@@ -0,0 +1,12 @@
+//alert("Hello, world!");
+
+//single line commment
+
+/*block
+    comment
+*/
+
+let message = "Hello, world!";
+const pi = 3.14;
+alert("the value of pi is " + pi);
+console.log(message);
